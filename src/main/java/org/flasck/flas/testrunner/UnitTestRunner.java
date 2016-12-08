2c53da981b544c70ec274c3f4cc225c0821021d8
Gareth Powell <gareth@ziniki.org>
2016-12-08 06:33:10 -0500

    Hacked in some possible approaches for the unit test runner

--- src/main/java/org/flasck/flas/testrunner/UnitTestRunner.java
+++ src/main/java/org/flasck/flas/testrunner/UnitTestRunner.java
@@ -5,38 +5,60 @@ import static org.junit.Assert.fail;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.flasck.flas.compiler.CompileResult;
 import org.flasck.flas.compiler.ScriptCompiler;
 import org.flasck.flas.errors.ErrorResultException;
+import org.zinutils.bytecode.BCEClassLoader;
+import org.zinutils.bytecode.ByteCodeEnvironment;
+import org.zinutils.collections.CollectionUtils;
+import org.zinutils.reflection.Reflection;
 import org.zinutils.utils.FileUtils;
 import org.zinutils.utils.MultiTextEmitter;
 
 public class UnitTestRunner {
+	private final MultiTextEmitter results;
+	private final List<Class<?>> toRun = new ArrayList<>();
+	private final BCEClassLoader loader;
 
-	public UnitTestRunner(MultiTextEmitter results, ScriptCompiler compiler, File f) throws IOException {
+	public UnitTestRunner(MultiTextEmitter results, ScriptCompiler compiler, ByteCodeEnvironment bce, String pkg, File f) throws IOException {
+		this.results = results;
+		loader = new BCEClassLoader(bce);
+
 		// I think:
 		// 1. Convert f into a standard fl "program" with a set of functions
 		//      and build a meta-repository of what's going on
 		UnitTestConvertor c = new UnitTestConvertor();
-		String pkg = f.getParentFile().getName();
-		TestScript script = c.convert(pkg, FileUtils.readFileAsLines(f));
+		TestScript script = c.convert(pkg+".script", FileUtils.readFileAsLines(f));
 
 		// 2. Compile this to JVM bytecodes using the regular compiler
 		// - should only have access to exported things
+		CompileResult cr = null;
 		try {
-			CompileResult cr = compiler.createJVM(pkg, script.flas);
-			System.out.println("cr = " + cr);
+			cr = compiler.createJVM(pkg+".script", script.flas);
+			System.out.println("cr = " + cr.bce.all());
 		} catch (ErrorResultException ex) {
 			ex.errors.showTo(new PrintWriter(System.err), 0);
 			fail("Errors compiling test script");
 		}
 		// 3. Load the class(es) into memory
-		// 4. store the emitter
+		cr.bce.dumpAll(true);
+		loader.add(cr.bce);
+		loader.defineClass(pkg + ".script.PACKAGEFUNCTIONS");
+		toRun.add(loader.defineClass(pkg + ".script.PACKAGEFUNCTIONS$expr1"));
+		loader.defineClass(pkg + ".script.PACKAGEFUNCTIONS$value1");
+		loader.defineClass(pkg + ".PACKAGEFUNCTIONS$x");
 	}
-
+	
 	public void run() {
 		// 5. Execute all the relevant functions & compare the results
+
+		for (Class<?> clz : toRun) {
+			System.out.println("Evaluating " + clz);
+			Reflection.callStatic(clz, "eval", new Object[] { new Object[] {} });
+		}
 	}
 
 }
--- src/test/java/test/flas/testrunner/SimpleUnitTestRunnerTests.java
+++ src/test/java/test/flas/testrunner/SimpleUnitTestRunnerTests.java
@@ -7,20 +7,51 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.flasck.flas.Main;
+import org.flasck.flas.blockForm.InputPosition;
+import org.flasck.flas.compiler.CompileResult;
 import org.flasck.flas.compiler.FLASCompiler;
+import org.flasck.flas.errors.ErrorResult;
+import org.flasck.flas.newtypechecker.TypeChecker2;
+import org.flasck.flas.rewriter.Rewriter;
 import org.flasck.flas.testrunner.UnitTestRunner;
+import org.flasck.flas.types.Type;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.zinutils.bytecode.ByteCodeCreator;
+import org.zinutils.bytecode.ByteCodeEnvironment;
 import org.zinutils.utils.MultiTextEmitter;
 
 public class SimpleUnitTestRunnerTests {
 
+	@BeforeClass
+	public static void prepareLogs() {
+		Main.setLogLevels();
+	}
+	
 	@Test
 	public void testItCanTestASimpleValue() throws Exception {
 		File f = createFile("\tvalue x", "\t\t32");
 		StringWriter sw = new StringWriter();
-		UnitTestRunner r = new UnitTestRunner(new MultiTextEmitter(sw), new FLASCompiler(), f);
+		FLASCompiler sc = new FLASCompiler();
+		ErrorResult errors = new ErrorResult();
+		Rewriter rw = new Rewriter(errors, null, null);
+		TypeChecker2 tc = new TypeChecker2(errors, rw);
+		InputPosition loc = new InputPosition("-", 1, 0, null);
+		tc.define("test.golden.x", Type.function(loc, Type.builtin(loc, "Number")));
+		ByteCodeEnvironment bce = new ByteCodeEnvironment();
+		defineX(bce);
+		bce.dumpAll(true);
+		CompileResult prior = new CompileResult("test.golden", bce, tc);
+		sc.includePrior(prior);
+		UnitTestRunner r = new UnitTestRunner(new MultiTextEmitter(sw), sc, prior.bce, prior.getPackage(), f);
 		r.run();
 		assertEquals("PASSED:\tvalue x\n", sw.toString());
+	}
+
+	private void defineX(ByteCodeEnvironment bce) {
+		ByteCodeCreator bcc = new ByteCodeCreator(bce, "test.golden.PACKAGEFUNCTIONS$x");
+		System.out.println(bce.all());
 	}
 
 	private File createFile(String... lines) throws IOException {
