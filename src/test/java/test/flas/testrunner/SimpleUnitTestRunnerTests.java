adfedf80cc04b03dfad4dd491c73e3c9809b0660
Gareth Powell <gareth@ziniki.org>
2016-12-09 12:32:55 -0500

    Added a second test for testrunner Introduced JMock Refactored to simplify things

--- src/test/java/test/flas/testrunner/SimpleUnitTestRunnerTests.java
+++ src/test/java/test/flas/testrunner/SimpleUnitTestRunnerTests.java
@@ -1,12 +1,8 @@
 package test.flas.testrunner;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.StringWriter;
-
 import org.flasck.flas.Main;
 import org.flasck.flas.blockForm.InputPosition;
 import org.flasck.flas.compiler.CompileResult;
@@ -16,56 +12,113 @@ import org.flasck.flas.newtypechecker.TypeChecker2;
 import org.flasck.flas.rewriter.Rewriter;
 import org.flasck.flas.testrunner.UnitTestRunner;
 import org.flasck.flas.types.Type;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.zinutils.bytecode.ByteCodeCreator;
 import org.zinutils.bytecode.ByteCodeEnvironment;
 import org.zinutils.bytecode.GenericAnnotator;
+import org.zinutils.bytecode.GenericAnnotator.PendingVar;
 import org.zinutils.bytecode.MethodDefiner;
+import org.zinutils.utils.LinePrinter;
 import org.zinutils.utils.MultiTextEmitter;
 
 public class SimpleUnitTestRunnerTests {
-
+	Mockery context = new Mockery();
+	InputPosition loc = new InputPosition("-", 1, 0, null);
+	FLASCompiler sc = new FLASCompiler();
+	ErrorResult errors = new ErrorResult();
+	Rewriter rw = new Rewriter(errors, null, null);
+	TypeChecker2 tc = new TypeChecker2(errors, rw);
+	ByteCodeEnvironment bce = new ByteCodeEnvironment();
+	CompileResult prior;
+	LinePrinter writer = context.mock(LinePrinter.class);
+	
 	@BeforeClass
 	public static void prepareLogs() {
 		Main.setLogLevels();
 	}
+
+	class Setup {
+		Setup() {
+			System.out.println("hello");
+		}
+		
+	}
+
+	private void go(Setup setup) {
+		defineSupportingFunctions(bce);
+		bce.dumpAll(true);
+		prior = new CompileResult("test.golden", bce, tc);
+		sc.includePrior(prior);
+	}
 	
 	@Test
 	public void testItCanTestASimpleValue() throws Exception {
-		File f = createFile("\tvalue x", "\t\t32");
-		StringWriter sw = new StringWriter();
-		FLASCompiler sc = new FLASCompiler();
-		ErrorResult errors = new ErrorResult();
-		Rewriter rw = new Rewriter(errors, null, null);
-		TypeChecker2 tc = new TypeChecker2(errors, rw);
-		InputPosition loc = new InputPosition("-", 1, 0, null);
-		tc.define("test.golden.x", Type.function(loc, Type.builtin(loc, "Number")));
-		ByteCodeEnvironment bce = new ByteCodeEnvironment();
-		defineX(bce);
-		bce.dumpAll(true);
-		CompileResult prior = new CompileResult("test.golden", bce, tc);
-		sc.includePrior(prior);
-		UnitTestRunner r = new UnitTestRunner(new MultiTextEmitter(sw), sc, prior.bce, prior.getPackage(), f);
-		r.run();
-		assertEquals("PASSED:\tvalue x\n", sw.toString());
+		go(new Setup() {{
+			tc.define("test.golden.x", Type.function(loc, Type.builtin(loc, "Number")));
+		}});
+		context.checking(new Expectations() {{
+			oneOf(writer).print("PASSED");
+			oneOf(writer).println(":\tvalue x");
+		}});
+
+		runTestScript("\tvalue x", "\t\t32");
 	}
 
-	private void defineX(ByteCodeEnvironment bce) {
+	@Test
+	public void testItCanTestATrivialFunctionCall() throws Exception {
+		go(new Setup() {{
+			Type varA = Type.polyvar(loc, "A");
+			tc.define("test.golden.id", Type.function(loc, varA, varA));
+		}});
+		context.checking(new Expectations() {{
+			oneOf(writer).print("PASSED");
+			oneOf(writer).println(":\tvalue x");
+		}});
+
+		runTestScript("\tvalue (id 'hello')", "\t\t'hello'");
+	}
+
+	private void runTestScript(String... lines) throws IOException {
+		File f = createFile(lines);
+		UnitTestRunner r = new UnitTestRunner(new MultiTextEmitter(writer), sc, prior.bce, prior.getPackage(), f);
+		r.run();
+	}
+
+	private void defineSupportingFunctions(ByteCodeEnvironment bce) {
 		{
 			ByteCodeCreator bcc = new ByteCodeCreator(bce, "test.golden.PACKAGEFUNCTIONS$x");
 			GenericAnnotator ga = GenericAnnotator.newMethod(bcc, true, "eval");
 			ga.argument("[java.lang.Object", "args");
 			ga.returns("java.lang.Object");
 			MethodDefiner meth = ga.done();
-			meth.returnObject(meth.callStatic("test.golden.PACKAGEFUNCTIONS", "java.lang.Object", "x")).flush();;
+			meth.returnObject(meth.callStatic("test.golden.PACKAGEFUNCTIONS", "java.lang.Object", "x")).flush();
+		}
+		{
+			ByteCodeCreator bcc = new ByteCodeCreator(bce, "test.golden.PACKAGEFUNCTIONS$id");
+			GenericAnnotator ga = GenericAnnotator.newMethod(bcc, true, "eval");
+			PendingVar args = ga.argument("[java.lang.Object", "args");
+			ga.returns("java.lang.Object");
+			MethodDefiner meth = ga.done();
+			meth.returnObject(meth.callStatic("test.golden.PACKAGEFUNCTIONS", "java.lang.Object", "id", meth.arrayElt(args.getVar(), meth.intConst(0)))).flush();
 		}
 		{
 			ByteCodeCreator bcc = new ByteCodeCreator(bce, "test.golden.PACKAGEFUNCTIONS");
-			GenericAnnotator ga = GenericAnnotator.newMethod(bcc, true, "x");
-			ga.returns("java.lang.Object");
-			MethodDefiner meth = ga.done();
-			meth.returnObject(meth.callStatic("java.lang.Integer", "java.lang.Integer", "valueOf", meth.intConst(32))).flush();;
+			{
+				GenericAnnotator ga = GenericAnnotator.newMethod(bcc, true, "x");
+				ga.returns("java.lang.Object");
+				MethodDefiner meth = ga.done();
+				meth.returnObject(meth.callStatic("java.lang.Integer", "java.lang.Integer", "valueOf", meth.intConst(32))).flush();
+			}
+			{
+				GenericAnnotator ga = GenericAnnotator.newMethod(bcc, true, "id");
+				PendingVar val = ga.argument("java.lang.Object", "val");
+				ga.returns("java.lang.Object");
+				MethodDefiner meth = ga.done();
+				meth.returnObject(val.getVar()).flush();
+			}
 		}
 		System.out.println(bce.all());
 	}
