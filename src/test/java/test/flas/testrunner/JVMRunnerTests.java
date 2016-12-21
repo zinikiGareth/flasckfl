a4e331e620da1daff529d7257b02bc939fb13da4
Gareth Powell <gareth@ziniki.org>
2016-12-21 10:30:58 -0500

    JVM tests work with latest flasjvm

--- src/test/java/test/flas/testrunner/JVMRunnerTests.java
+++ src/test/java/test/flas/testrunner/JVMRunnerTests.java
@@ -15,6 +15,8 @@ import org.zinutils.bytecode.JavaInfo.Access;
 import org.zinutils.bytecode.JavaType;
 import org.zinutils.bytecode.MethodDefiner;
 import org.zinutils.bytecode.NewMethodDefiner;
+import org.zinutils.bytecode.Remarker;
+import org.zinutils.bytecode.StackMapFrame;
 import org.zinutils.bytecode.Var;
 
 public class JVMRunnerTests extends BaseRunnerTests {
@@ -89,6 +91,19 @@ public class JVMRunnerTests extends BaseRunnerTests {
 				ctor.returnVoid().flush();
 			}
 			{
+				GenericAnnotator ann = GenericAnnotator.newMethod(bcc, false, "setOff");
+				ann.argument("org.flasck.jvm.post.DeliveryAddress", "from");
+				ann.returns(JavaType.object_);
+				NewMethodDefiner meth = ann.done();
+				Var clos1 = meth.avar("org.flasck.jvm.FLClosure", "clos1");
+				meth.assign(clos1, meth.makeNew("org.flasck.jvm.FLClosure",
+										meth.classConst("org.flasck.jvm.builtin.Assign"),
+										meth.arrayOf("java.lang.Object", Arrays.asList(meth.getField("_card"), meth.stringConst("sayHello"), meth.as(meth.makeNew("java.lang.Boolean", meth.boolConst(false)), "java.lang.Object"))))).flush();
+				meth.returnObject(meth.makeNew("org.flasck.jvm.FLClosure", 
+										meth.classConst("org.flasck.jvm.builtin.Cons"),
+										meth.arrayOf("java.lang.Object", Arrays.asList(clos1, meth.callStatic("org.flasck.jvm.builtin.Nil", "java.lang.Object", "eval", meth.arrayOf("java.lang.Object", new ArrayList<>())))))).flush();
+			}
+			{
 				GenericAnnotator ann = GenericAnnotator.newMethod(bcc, false, "setOn");
 				ann.argument("org.flasck.jvm.post.DeliveryAddress", "from");
 				ann.returns(JavaType.object_);
@@ -101,27 +116,13 @@ public class JVMRunnerTests extends BaseRunnerTests {
 										meth.classConst("org.flasck.jvm.builtin.Cons"),
 										meth.arrayOf("java.lang.Object", Arrays.asList(clos1, meth.callStatic("org.flasck.jvm.builtin.Nil", "java.lang.Object", "eval", meth.arrayOf("java.lang.Object", new ArrayList<>())))))).flush();
 			}
-			bcc.writeTo(new File("/Users/gareth/bcc.class"));
-		}
-		{
-			ByteCodeCreator bcc = new ByteCodeCreator(bce, "test.runner.Card$B1");
-			bcc.superclass("org.flasck.jvm.areas.TextArea");
-			{
-				GenericAnnotator ann = GenericAnnotator.newConstructor(bcc, false);
-				ann.argument("test.runner.Card", "card");
-				PendingVar parent = ann.argument("org.flasck.jvm.areas.CardArea", "_parent");
-				MethodDefiner ctor = ann.done();
-				ctor.callSuper("void", "org.flasck.jvm.areas.TextArea", "<init>", ctor.as(parent.getVar(), "org.flasck.jvm.areas.Area"), ctor.as(ctor.aNull(), "java.lang.String")).flush();
-				ctor.callVirtual("void", ctor.myThis(), "_setText", ctor.stringConst("hello, world")).flush();
-				ctor.returnVoid().flush();
-			}
 		}
 		{
 			ByteCodeCreator bcc = new ByteCodeCreator(bce, "test.runner.Card");
+			bcc.superclass("org.flasck.android.FlasckActivity");
 			bcc.inheritsField(true, Access.PROTECTED, new JavaType("org.flasck.jvm.ActivityDelegate"), "_delegate");
 			bcc.inheritsField(true, Access.PROTECTED, new JavaType("org.flasck.jdk.display.JDKDisplay"), "_display");
 			bcc.defineField(false, Access.PROTECTED, JavaType.boolean_, "sayHello");
-			bcc.superclass("org.flasck.android.FlasckActivity");
 			{
 				GenericAnnotator ann = GenericAnnotator.newConstructor(bcc, false);
 				MethodDefiner ctor = ann.done();
@@ -146,6 +147,78 @@ public class JVMRunnerTests extends BaseRunnerTests {
 				NewMethodDefiner meth = ann.done();
 				meth.makeNewVoid("test.runner.Card$B1", meth.myThis(), meth.makeNew("org.flasck.jvm.areas.CardArea", meth.getField(meth.getField("_delegate"), "wrapper"), meth.as(meth.getField("_display"), "org.flasck.jvm.display.DisplayEngine"), into.getVar())).flush();
 				meth.returnVoid().flush();
+			}
+			{
+				GenericAnnotator ann = GenericAnnotator.newMethod(bcc, false, "styleIf");
+				PendingVar str = ann.argument("java.lang.Object", "str");
+				PendingVar bool = ann.argument("java.lang.Object", "bool");
+				ann.returns("java.lang.Object");
+				NewMethodDefiner meth = ann.done();
+				Remarker m1 = new Remarker();
+				meth.ifBoolean(meth.unbox(meth.castTo(bool.getVar(), "java.lang.Boolean"), false), meth.returnObject(str.getVar()), meth.block(meth.markHere(m1), meth.returnObject(meth.stringConst("")))).flush();
+				meth.addStackMapFrame(StackMapFrame.SAME_FRAME, m1.getMarker());
+			}
+			bcc.writeTo(new File("/Users/gareth/bcc.class"));
+		}
+		{
+			ByteCodeCreator bcc = new ByteCodeCreator(bce, "test.runner.Card$B1");
+			bcc.superclass("org.flasck.jvm.areas.TextArea");
+			bcc.inheritsField(true, Access.PROTECTED, new JavaType("org.flasck.jvm.Wrapper"), "_wrapper");
+			bcc.defineField(true, Access.PROTECTED, "test.runner.Card", "_card");
+			{
+				GenericAnnotator ann = GenericAnnotator.newConstructor(bcc, false);
+				PendingVar card = ann.argument("test.runner.Card", "card");
+				PendingVar parent = ann.argument("org.flasck.jvm.areas.CardArea", "parent");
+				MethodDefiner ctor = ann.done();
+				ctor.callSuper("void", "org.flasck.jvm.areas.TextArea", "<init>", ctor.as(parent.getVar(), "org.flasck.jvm.areas.Area"), ctor.as(ctor.aNull(), "java.lang.String")).flush();
+				ctor.assign(ctor.getField("_card"), card.getVar()).flush();
+				ctor.callVirtual("void", ctor.myThis(), "_setText", ctor.stringConst("hello, world")).flush();
+				ctor.callVirtual("void", ctor.getField("_wrapper"), "onAssign", ctor.as(ctor.getField("_card"), "java.lang.Object"), ctor.stringConst("sayHello"), ctor.as(ctor.myThis(), "org.flasck.jvm.areas.Area"), ctor.stringConst("_setVariableFormats")).flush();
+				ctor.voidExpr(ctor.callVirtual("java.lang.Object", ctor.myThis(), "_setVariableFormats")).flush();
+				ctor.returnVoid().flush();
+			}
+			{
+				GenericAnnotator ann = GenericAnnotator.newMethod(bcc, false, "_setVariableFormats");
+				ann.returns(JavaType.object_);
+				NewMethodDefiner meth = ann.done();
+				meth.callSuper("void", "org.flasck.jvm.areas.TextArea", "_setCSSObj", meth.callVirtual("java.lang.Object", meth.myThis(), "formats_0")).flush();
+				meth.returnObject(meth.aNull()).flush();
+			}
+			{
+				GenericAnnotator ann = GenericAnnotator.newMethod(bcc, false, "formats_0");
+				ann.returns(JavaType.object_);
+				NewMethodDefiner meth = ann.done();
+				Var clos1 = meth.avar("org.flasck.jvm.FLClosure", "clos1");
+				meth.assign(clos1, meth.makeNew("org.flasck.jvm.FLClosure",
+										meth.as(meth.getField("_card"), "java.lang.Object"),
+										meth.classConst("test.runner.Card$styleIf"),
+										meth.arrayOf("java.lang.Object", Arrays.asList(meth.stringConst("show"), meth.callStatic("java.lang.Boolean", "java.lang.Boolean", "valueOf", meth.getField(meth.getField("_card"), "sayHello")))))).flush();
+				meth.returnObject(meth.makeNew("org.flasck.jvm.FLClosure", 
+										meth.classConst("org.flasck.jvm.builtin.Cons"),
+										meth.arrayOf("java.lang.Object", Arrays.asList(clos1, meth.callStatic("org.flasck.jvm.builtin.Nil", "java.lang.Object", "eval", meth.arrayOf("java.lang.Object", new ArrayList<>())))))).flush();
+			}
+		}
+		{
+			ByteCodeCreator bcc = new ByteCodeCreator(bce, "test.runner.Card$styleIf");
+			bcc.superclass("java.lang.Object");
+			bcc.defineField(true, Access.PROTECTED, "test.runner.Card", "_card");
+			{
+				GenericAnnotator ann = GenericAnnotator.newConstructor(bcc, false);
+				PendingVar card = ann.argument("test.runner.Card", "card");
+				MethodDefiner ctor = ann.done();
+				ctor.callSuper("void", "java.lang.Object", "<init>").flush();
+				ctor.assign(ctor.getField("_card"), card.getVar()).flush();
+				ctor.returnVoid().flush();
+			}
+			{
+				GenericAnnotator ann = GenericAnnotator.newMethod(bcc, true, "eval");
+				PendingVar meP = ann.argument(new JavaType("java.lang.Object"), "me");
+				PendingVar argsP = ann.argument(new JavaType("[java.lang.Object"), "args");
+				ann.returns(JavaType.object_);
+				NewMethodDefiner meth = ann.done();
+				Var me = meP.getVar();
+				Var args = argsP.getVar();
+				meth.returnObject(meth.callVirtual("java.lang.Object", meth.castTo(me, "test.runner.Card"), "styleIf", meth.arrayElt(args, meth.intConst(0)), meth.arrayElt(args, meth.intConst(1)))).flush();
 			}
 		}
 	}
