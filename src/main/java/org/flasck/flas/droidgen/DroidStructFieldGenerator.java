a1d167901efe53e22d09ec12090979d0239234dc
Gareth Powell <gareth@ziniki.org>
2017-02-09 18:02:10 -0500

    Extracted a TypeMapper for all situations

--- src/main/java/org/flasck/flas/droidgen/DroidStructFieldGenerator.java
+++ src/main/java/org/flasck/flas/droidgen/DroidStructFieldGenerator.java
@@ -5,15 +5,9 @@ import java.util.TreeMap;
 
 import org.flasck.flas.rewrittenForm.FieldVisitor;
 import org.flasck.flas.rewrittenForm.RWStructField;
-import org.flasck.flas.types.FunctionType;
-import org.flasck.flas.types.PolyVar;
-import org.flasck.flas.types.PrimitiveType;
-import org.flasck.flas.types.TypeWithName;
 import org.zinutils.bytecode.ByteCodeSink;
 import org.zinutils.bytecode.IFieldInfo;
-import org.zinutils.bytecode.JavaType;
 import org.zinutils.bytecode.JavaInfo.Access;
-import org.zinutils.exceptions.UtilException;
 
 public class DroidStructFieldGenerator implements FieldVisitor {
 	private final ByteCodeSink bcc;
@@ -27,26 +21,7 @@ public class DroidStructFieldGenerator implements FieldVisitor {
 
 	@Override
 	public void visit(RWStructField sf) {
-		JavaType jt;
-		if (sf.type instanceof PrimitiveType) {
-			PrimitiveType pt = (PrimitiveType)sf.type;
-			if (pt.name().equals("Number"))
-				jt = JavaType.int_; // what about floats?
-			else if (pt.name().equals("String"))
-				jt = JavaType.string;
-			else if (pt.name().equals("Boolean"))
-				jt = JavaType.boolean_;
-			else
-				throw new UtilException("Not handled " + sf.type);
-		} else if (sf.type instanceof FunctionType) {
-			jt = JavaType.object_;
-		} else if (sf.type instanceof PolyVar) {
-			jt = JavaType.object_;
-		} else if (sf.type instanceof TypeWithName) {
-			jt = new JavaType(((TypeWithName)sf.type).getName().javaName());
-		} else
-			throw new UtilException("Not handled " + sf.type + " " + sf.type.getClass());
-		IFieldInfo fi = bcc.defineField(false, access, jt, sf.name);
+		IFieldInfo fi = bcc.defineField(false, access, JvmTypeMapper.map(sf.type), sf.name);
 		fields.put(sf.name, fi);
 	}
 }
---
+++ src/main/java/org/flasck/flas/droidgen/JvmTypeMapper.java
@@ -0,0 +1,37 @@
+package org.flasck.flas.droidgen;
+
+import org.flasck.flas.types.FunctionType;
+import org.flasck.flas.types.PolyVar;
+import org.flasck.flas.types.PrimitiveType;
+import org.flasck.flas.types.Type;
+import org.flasck.flas.types.TypeWithName;
+import org.flasck.jvm.post.TypeOf;
+import org.zinutils.bytecode.JavaType;
+import org.zinutils.exceptions.UtilException;
+
+public class JvmTypeMapper {
+
+	public static JavaType map(Type type) {
+		if (type instanceof PrimitiveType) {
+			PrimitiveType pt = (PrimitiveType)type;
+			if (pt.name().equals("Number"))
+				return JavaType.int_; // what about floats?
+			else if (pt.name().equals("String"))
+				return JavaType.string;
+			else if (pt.name().equals("Boolean"))
+				return JavaType.boolean_;
+			else if (pt.name().equals("Type"))
+				return new JavaType(TypeOf.class.getName());
+			else
+				throw new UtilException("Not handled " + type);
+		} else if (type instanceof FunctionType) {
+			return JavaType.object_;
+		} else if (type instanceof PolyVar) {
+			return JavaType.object_;
+		} else if (type instanceof TypeWithName) {
+			return new JavaType(((TypeWithName)type).getName().javaName());
+		} else
+			throw new UtilException("Not handled " + type + " " + type.getClass());
+	}
+
+}
