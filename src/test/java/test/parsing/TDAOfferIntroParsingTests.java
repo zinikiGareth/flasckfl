8d12a2dbd70d8d41aa48766a461c588d8c9fbcd0
Gareth Powell <gareth@ziniki.org>
2018-11-14 17:32:38 -0500

    Offer support

---
+++ src/test/java/test/parsing/TDAOfferIntroParsingTests.java
@@ -0,0 +1,59 @@
+package test.parsing;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.flasck.flas.commonBase.names.SolidName;
+import org.flasck.flas.errors.ErrorReporter;
+import org.flasck.flas.parsedForm.StructDefn.StructType;
+import org.flasck.flas.parser.IgnoreNestedParser;
+import org.flasck.flas.parser.TDAIntroParser;
+import org.flasck.flas.parser.TDAParsing;
+import org.flasck.flas.parser.TDAStructFieldParser;
+import org.flasck.flas.parser.TopLevelDefnConsumer;
+import org.flasck.flas.tokenizers.Tokenizable;
+import org.jmock.Expectations;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class TDAOfferIntroParsingTests {
+	@Rule public JUnitRuleMockery context = new JUnitRuleMockery();
+	private ErrorReporter errors = context.mock(ErrorReporter.class);
+	private TopLevelDefnConsumer builder = context.mock(TopLevelDefnConsumer.class);
+
+	@Test
+	public void theSimplestOfferCreatesAScopeEntryAndReturnsAFieldParser() {
+		context.checking(new Expectations() {{
+			allowing(builder).qualifyName("Coffee"); will(returnValue(new SolidName(null, "Coffee")));
+			oneOf(builder).newStruct(with(StructDefnMatcher.match("Coffee").as(StructType.OFFER)));
+		}});
+		TDAIntroParser parser = new TDAIntroParser(errors, builder);
+		TDAParsing nested = parser.tryParsing(TDABasicIntroParsingTests.line("offer Coffee"));
+		assertTrue(nested instanceof TDAStructFieldParser);
+	}
+
+	@Test
+	public void thereMustBeATypeName() {
+		Tokenizable toks = TDABasicIntroParsingTests.line("offer");
+		context.checking(new Expectations() {{
+			oneOf(errors).message(toks, "invalid or missing type name");
+		}});
+		TDAIntroParser parser = new TDAIntroParser(errors, builder);
+		TDAParsing nested = parser.tryParsing(toks);
+		assertNotNull(nested);
+		assertTrue(nested instanceof IgnoreNestedParser);
+	}
+
+	@Test
+	public void theTypeNameMustBeTheValidKind() {
+		Tokenizable toks = TDABasicIntroParsingTests.line("offer fred");
+		context.checking(new Expectations() {{
+			oneOf(errors).message(toks, "invalid or missing type name");
+		}});
+		TDAIntroParser parser = new TDAIntroParser(errors, builder);
+		TDAParsing nested = parser.tryParsing(toks);
+		assertNotNull(nested);
+		assertTrue(nested instanceof IgnoreNestedParser);
+	}
+}
