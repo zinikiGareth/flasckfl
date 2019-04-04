1797b7078ae3fd8f84b207453143b1fadcdc00fa
Gareth Powell <gareth@ziniki.org>
2019-04-04 18:09:42 +0100

    Added one more file

---
+++ src/main/java/org/flasck/flas/parser/TDACardElementsParser.java
@@ -0,0 +1,20 @@
+package org.flasck.flas.parser;
+
+import org.flasck.flas.errors.ErrorReporter;
+import org.flasck.flas.tokenizers.Tokenizable;
+
+public class TDACardElementsParser implements TDAParsing {
+	private final ErrorReporter errors;
+	private final ParsedLineConsumer consumer;
+
+	public TDACardElementsParser(ErrorReporter errors, ParsedLineConsumer consumer) {
+		this.errors = errors;
+		this.consumer = consumer;
+	}
+
+	@Override
+	public TDAParsing tryParsing(Tokenizable toks) {
+		throw new org.zinutils.exceptions.NotImplementedException();
+	}
+
+}
