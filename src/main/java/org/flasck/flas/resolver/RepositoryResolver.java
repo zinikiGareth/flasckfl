c5f2dac5de246a995926bfc1607f7242b70bebc4
Gareth Powell <gareth@ziniki.org>
2019-06-19 07:03:42 -0400

    Handled error case

--- src/main/java/org/flasck/flas/resolver/RepositoryResolver.java
+++ src/main/java/org/flasck/flas/resolver/RepositoryResolver.java
@@ -1,19 +1,28 @@
 package org.flasck.flas.resolver;
 
+import org.flasck.flas.errors.ErrorReporter;
 import org.flasck.flas.parsedForm.UnresolvedOperator;
 import org.flasck.flas.parsedForm.UnresolvedVar;
+import org.flasck.flas.repository.RepositoryEntry;
 import org.flasck.flas.repository.RepositoryReader;
 
 public class RepositoryResolver implements Resolver {
+	private final ErrorReporter errors;
 	private final RepositoryReader repository;
 
-	public RepositoryResolver(RepositoryReader repository) {
+	public RepositoryResolver(ErrorReporter errors, RepositoryReader repository) {
+		this.errors = errors;
 		this.repository = repository;
 	}
 
 	@Override
 	public void visitUnresolvedVar(UnresolvedVar var) {
-		var.bind(repository.get("test.repo.f"));
+		final RepositoryEntry defn = repository.get("test.repo.f");
+		if (defn == null) {
+			errors.message(var.location, "cannot resolve '" + var.var + "'");
+			return;
+		}
+		var.bind(defn);
 	}
 
 	@Override
--- src/test/java/test/resolver/ResolverTests.java
+++ src/test/java/test/resolver/ResolverTests.java
@@ -1,19 +1,26 @@
 package test.resolver;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.flasck.flas.blockForm.InputPosition;
 import org.flasck.flas.commonBase.names.FunctionName;
 import org.flasck.flas.commonBase.names.PackageName;
+import org.flasck.flas.errors.ErrorReporter;
 import org.flasck.flas.parsedForm.FunctionDefinition;
 import org.flasck.flas.parsedForm.UnresolvedOperator;
 import org.flasck.flas.parsedForm.UnresolvedVar;
 import org.flasck.flas.repository.Repository;
 import org.flasck.flas.resolver.RepositoryResolver;
 import org.flasck.flas.resolver.Resolver;
+import org.jmock.Expectations;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class ResolverTests {
+	@Rule public JUnitRuleMockery context = new JUnitRuleMockery();
+	private ErrorReporter errors = context.mock(ErrorReporter.class);
+//	private LocalErrorTracker tracker = new LocalErrorTracker(errors);
 	private InputPosition pos = new InputPosition("-", 1, 0, "hello");
 	private final PackageName pkg = new PackageName("test.repo");
 	private final FunctionName nameF = FunctionName.function(pos, pkg, "f");
@@ -24,7 +31,7 @@ public class ResolverTests {
 		Repository ry = new Repository();
 		final FunctionDefinition fn = new FunctionDefinition(nameF, 2);
 		ry.functionDefn(fn);
-		Resolver r = new RepositoryResolver(ry);
+		Resolver r = new RepositoryResolver(errors, ry);
 		final UnresolvedVar var = new UnresolvedVar(pos, "f");
 		r.visitUnresolvedVar(var);
 		assertEquals(fn, var.defn());
@@ -35,10 +42,21 @@ public class ResolverTests {
 		Repository ry = new Repository();
 		final FunctionDefinition fn = new FunctionDefinition(namePlPl, 2);
 		ry.functionDefn(fn);
-		Resolver r = new RepositoryResolver(ry);
+		Resolver r = new RepositoryResolver(errors, ry);
 		final UnresolvedOperator var = new UnresolvedOperator(pos, "++");
 		r.visitUnresolvedOperator(var);
 		assertEquals(fn, var.defn());
+	}
+
+	@Test
+	public void anUndefinedNameCantBeResolved() {
+		context.checking(new Expectations() {{
+			oneOf(errors).message(pos, "cannot resolve 'f'");
+		}});
+		Repository ry = new Repository();
+		Resolver r = new RepositoryResolver(errors, ry);
+		final UnresolvedVar var = new UnresolvedVar(pos, "f");
+		r.visitUnresolvedVar(var);
 	}
 
 }
