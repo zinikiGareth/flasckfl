555a8f4bc371c8cb8f1f761254fed6c554335f24
Gareth Powell <gareth@ziniki.org>
2019-10-09 08:29:12 +0200

    Added first failing big-unit test

--- src/test/java/test/lifting/CollectingNestedVariableReferences.java
+++ src/test/java/test/lifting/CollectingNestedVariableReferences.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.flasck.flas.blockForm.InputPosition;
+import org.flasck.flas.commonBase.StringLiteral;
 import org.flasck.flas.commonBase.names.FunctionName;
 import org.flasck.flas.commonBase.names.PackageName;
 import org.flasck.flas.commonBase.names.VarName;
@@ -17,6 +18,7 @@ import org.flasck.flas.parsedForm.VarPattern;
 import org.flasck.flas.repository.Repository;
 import org.flasck.flas.repository.Repository.Visitor;
 import org.jmock.Expectations;
+import org.jmock.Sequence;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
@@ -45,11 +47,50 @@ public class CollectingNestedVariableReferences {
 		r.addEntry(name, fn);
 		l.lift(r);
 		
+		Sequence seq = context.sequence("order");
 		context.checking(new Expectations() {{
-			oneOf(v).visitFunction(fn);
-			oneOf(v).visitPatternVar(pos, "x");
+			oneOf(v).visitFunction(fn); inSequence(seq);
+			oneOf(v).visitPatternVar(pos, "x"); inSequence(seq);
 		}});
 		r.traverse(new LiftTestVisitor(v));
 	}
 
+	@Test
+	public void parentReferenceIsAddedToADirectChildUsingIt() {
+		FunctionName nameF = FunctionName.function(pos, pkg, "f");
+		VarPattern vp = new VarPattern(pos, new VarName(pos, nameF, "x"));
+
+		FunctionDefinition fnF = new FunctionDefinition(nameF, 1);
+		{
+			List<Object> args = new ArrayList<>();
+			args.add(vp);
+			FunctionIntro fi = new FunctionIntro(nameF, args);
+			FunctionCaseDefn fcd = new FunctionCaseDefn(null, new StringLiteral(pos, "hello"));
+			fi.functionCase(fcd);
+			fnF.intro(fi);
+			r.addEntry(nameF, fnF);
+		}
+		FunctionName nameG = FunctionName.function(pos, nameF, "g");
+		FunctionDefinition fnG = new FunctionDefinition(nameG, 0);
+		{
+			UnresolvedVar vr = new UnresolvedVar(pos, "x");
+			vr.bind(vp);
+			FunctionIntro fi = new FunctionIntro(nameG, new ArrayList<>());
+			FunctionCaseDefn fcd = new FunctionCaseDefn(null, vr);
+			fi.functionCase(fcd);
+			fnG.intro(fi);
+			r.addEntry(nameG, fnG);
+		}
+		
+		l.lift(r);
+		
+		Sequence seq = context.sequence("order");
+		context.checking(new Expectations() {{
+			oneOf(v).visitFunction(fnF); inSequence(seq);
+			oneOf(v).visitPatternVar(pos, "x"); inSequence(seq);
+			oneOf(v).visitFunction(fnG); inSequence(seq);
+			oneOf(v).visitPatternVar(pos, "x"); inSequence(seq);
+		}});
+		r.traverse(new LiftTestVisitor(v));
+	}
 }
