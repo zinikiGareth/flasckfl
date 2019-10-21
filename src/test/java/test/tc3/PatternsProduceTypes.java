d7777326b53ad7c037e3ba18259fcddbfb48003a
Gareth Powell <gareth@ziniki.org>
2019-10-21 12:47:35 +0100

    Fixed pattern generation tests

--- src/test/java/test/tc3/PatternsProduceTypes.java
+++ src/test/java/test/tc3/PatternsProduceTypes.java
@@ -1,8 +1,5 @@
 package test.tc3;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
 import java.util.ArrayList;
 
 import org.flasck.flas.blockForm.InputPosition;
@@ -10,6 +7,8 @@ import org.flasck.flas.commonBase.ConstPattern;
 import org.flasck.flas.commonBase.names.FunctionName;
 import org.flasck.flas.commonBase.names.PackageName;
 import org.flasck.flas.errors.ErrorReporter;
+import org.flasck.flas.hsi.ArgSlot;
+import org.flasck.flas.hsi.Slot;
 import org.flasck.flas.parsedForm.FunctionDefinition;
 import org.flasck.flas.parsedForm.FunctionIntro;
 import org.flasck.flas.patterns.HSIArgsTree;
@@ -18,17 +17,19 @@ import org.flasck.flas.repository.NestedVisitor;
 import org.flasck.flas.repository.RepositoryReader;
 import org.flasck.flas.tc3.CurrentTCState;
 import org.flasck.flas.tc3.ExpressionChecker;
+import org.flasck.flas.tc3.ExpressionChecker.ExprResult;
 import org.flasck.flas.tc3.FunctionChecker;
-import org.flasck.flas.tc3.GroupChecker;
+import org.flasck.flas.tc3.FunctionChecker.ArgResult;
+import org.flasck.flas.tc3.SlotChecker;
+import org.hamcrest.Matchers;
 import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 // These are not really "unit" types because they are covering too much ground
 // To do what they purport to do, you need to cut into the pattern analyzer and see the type constraints that come out using visitInTheTCWay ...
-@Ignore
 public class PatternsProduceTypes {
 	@Rule public JUnitRuleMockery context = new JUnitRuleMockery();
 	private InputPosition pos = new InputPosition("-", 1, 0, "hello");
@@ -38,14 +39,18 @@ public class PatternsProduceTypes {
 	private final NestedVisitor sv = context.mock(NestedVisitor.class);
 	private final CurrentTCState state = context.mock(CurrentTCState.class);
 	final FunctionName nameF = FunctionName.function(pos, pkg, "fred");
-
-	@Test
-	public void aConstantPatternIsANumber() {
+	
+	@Before
+	public void begin() {
 		context.checking(new Expectations() {{
-			oneOf(sv).push(with(any(FunctionChecker.class)));
 			allowing(state);
 		}});
-		GroupChecker tc = new GroupChecker(errors, repository, sv, state);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void aConstantPatternIsANumber() {
+		FunctionChecker fc = new FunctionChecker(errors, repository, sv, state);
 		FunctionDefinition fn = new FunctionDefinition(nameF, 1);
 		ArrayList<Object> args = new ArrayList<>();
 		args.add(new ConstPattern(pos, ConstPattern.INTEGER, "42"));
@@ -56,22 +61,30 @@ public class PatternsProduceTypes {
 		fi.bindTree(hat);
 		fn.intro(fi);
 		fn.bindHsi(hat);
-		tc.visitFunction(fn);
-		tc.visitFunctionIntro(fi);
-		tc.result(LoadBuiltins.number);
-		tc.leaveFunctionIntro(fi);
-		tc.leaveFunction(fn);
-		assertNotNull("no type was bound", fn.type());
-		assertEquals("Number->Number", fn.type().signature());
-	}
-
-	@Test
-	public void aStringConstantPatternIsAString() {
+		fc.visitFunction(fn);
+		context.checking(new Expectations() {{
+			oneOf(sv).push(with(any(SlotChecker.class)));
+		}});
+		Slot s = new ArgSlot(0, hat.get(0));
+		fc.argSlot(s);
+		fc.result(new ArgResult(LoadBuiltins.number));
 		context.checking(new Expectations() {{
 			oneOf(sv).push(with(any(ExpressionChecker.class)));
-			allowing(state);
 		}});
-		GroupChecker tc = new GroupChecker(errors, repository, sv, state);
+		fc.visitFunctionIntro(fi);
+		context.assertIsSatisfied();
+		fc.result(new ExprResult(LoadBuiltins.number));
+		fc.leaveFunctionIntro(fi);
+		context.checking(new Expectations() {{
+			oneOf(sv).result(with(ApplyMatcher.type(Matchers.is(LoadBuiltins.number), Matchers.is(LoadBuiltins.number))));
+		}});
+		fc.leaveFunction(fn);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void aStringConstantPatternIsAString() {
+		FunctionChecker fc = new FunctionChecker(errors, repository, sv, state);
 		FunctionDefinition fn = new FunctionDefinition(nameF, 1);
 		ArrayList<Object> args = new ArrayList<>();
 		args.add(new ConstPattern(pos, ConstPattern.INTEGER, "42"));
@@ -82,12 +95,22 @@ public class PatternsProduceTypes {
 		fi.bindTree(hat);
 		fn.intro(fi);
 		fn.bindHsi(hat);
-		tc.visitFunction(fn);
-		tc.visitFunctionIntro(fi);
-		tc.result(LoadBuiltins.number);
-		tc.leaveFunctionIntro(fi);
-		tc.leaveFunction(fn);
-		assertNotNull("no type was bound", fn.type());
-		assertEquals("String->Number", fn.type().signature());
+		fc.visitFunction(fn);
+		context.checking(new Expectations() {{
+			oneOf(sv).push(with(any(SlotChecker.class)));
+		}});
+		Slot s = new ArgSlot(0, hat.get(0));
+		fc.argSlot(s);
+		fc.result(new ArgResult(LoadBuiltins.string));
+		context.checking(new Expectations() {{
+			oneOf(sv).push(with(any(ExpressionChecker.class)));
+		}});
+		fc.visitFunctionIntro(fi);
+		fc.result(new ExprResult(LoadBuiltins.number));
+		fc.leaveFunctionIntro(fi);
+		context.checking(new Expectations() {{
+			oneOf(sv).result(with(ApplyMatcher.type(Matchers.is(LoadBuiltins.string), Matchers.is(LoadBuiltins.number))));
+		}});
+		fc.leaveFunction(fn);
 	}
 }
