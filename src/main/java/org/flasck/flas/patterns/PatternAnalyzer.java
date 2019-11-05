127cd2cc8f517a373a6cdae08451cd6bffb89ae0
Gareth Powell <gareth@ziniki.org>
2019-11-05 05:37:29 -0500

    Added more complex cases

--- src/main/java/org/flasck/flas/patterns/PatternAnalyzer.java
+++ src/main/java/org/flasck/flas/patterns/PatternAnalyzer.java
@@ -83,7 +83,8 @@ public class PatternAnalyzer extends LeafAdapter {
 	@Override
 	public void visitConstructorMatch(ConstructorMatch p, boolean isNested) {
 		HSITree nested = slot.requireCM(p.actual());
-		nested.consider(current);
+		if (current != null)
+			nested.consider(current);
 		new PatternAnalyzer(errors, repository, sv, nested, current);
 	}
 	
