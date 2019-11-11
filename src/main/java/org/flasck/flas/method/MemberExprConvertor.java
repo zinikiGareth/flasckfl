96e4a6951bd604b70456220d8ecb7a47c20b6505
Gareth Powell <gareth@ziniki.org>
2019-11-11 17:57:22 -0500

    Hack to get around issues with dot and fields

--- src/main/java/org/flasck/flas/method/MemberExprConvertor.java
+++ src/main/java/org/flasck/flas/method/MemberExprConvertor.java
@@ -2,6 +2,7 @@ package org.flasck.flas.method;
 
 import org.flasck.flas.commonBase.Expr;
 import org.flasck.flas.commonBase.MemberExpr;
+import org.flasck.flas.commonBase.StringLiteral;
 import org.flasck.flas.commonBase.names.FunctionName;
 import org.flasck.flas.parsedForm.ContractDecl;
 import org.flasck.flas.parsedForm.ContractMethodDecl;
@@ -24,30 +25,41 @@ public class MemberExprConvertor extends LeafAdapter {
 		this.nv = nv;
 	}
 	
-	// TODO: this needs to collect the two parameters
 	@Override
 	public void visitUnresolvedVar(UnresolvedVar var, int nargs) {
 		if (obj == null) {
 			obj = var;
+			// TODO: this just "assumes" the method case; we also need to consider the field case
 			this.cd = resolveContract(var.defn());
 		} else if (sendMeth == null) {
-			cmd = this.cd.getMethod(var.var);
-			if (cmd == null)
-				throw new NotImplementedException("there is no method " + var.var + " on " + cd.name().uniqueName()); // REAL USER ERROR
-			sendMeth = cmd.name;
+			if (cd != null) {
+				cmd = this.cd.getMethod(var.var);
+				if (cmd == null)
+					throw new NotImplementedException("there is no method " + var.var + " on " + cd.name().uniqueName()); // REAL USER ERROR
+				sendMeth = cmd.name;
+			} else {
+				sendMeth = FunctionName.contractMethod(var.location(), null, "handle_the_field_case");
+			}
 		}
 	}
 
 	private ContractDecl resolveContract(RepositoryEntry defn) {
 		if (defn instanceof TypedPattern) {
 			TypedPattern tp = (TypedPattern) defn;
-			return (ContractDecl) tp.type.defn();
+			RepositoryEntry dt = tp.type.defn();
+			if (dt instanceof ContractDecl)
+				return (ContractDecl) tp.type.defn();
+			else
+				return null;
 		} else
 			throw new NotImplementedException("cannot handle svc of type " + (defn == null ? "NULL" : defn.getClass()));
 	}
 
 	@Override
 	public void leaveMemberExpr(MemberExpr expr) {
-		nv.result(new MakeSend(expr.location(), sendMeth, obj, cmd.args.size()));
+		if (cmd != null)
+			nv.result(new MakeSend(expr.location(), sendMeth, obj, cmd.args.size()));
+		else
+			nv.result(new StringLiteral(expr.location(), "need_to_implement_field_case"));
 	}
 }
