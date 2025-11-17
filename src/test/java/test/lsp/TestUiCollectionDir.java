42caac3a38b94cb590cbff5707158ddb16bb0c7c
Gareth Powell <gareth@ziniki.org>
2025-11-17 14:50:08 +0000

    Added test case for one file

--- src/test/java/test/lsp/TestUiCollectionDir.java
+++ src/test/java/test/lsp/TestUiCollectionDir.java
@@ -1,15 +1,25 @@
 package test.lsp;
 
+import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.flasck.flas.compiler.CompileUnit;
+import org.flasck.flas.errors.ErrorReporter;
 import org.flasck.flas.lsp.Root;
+import org.jmock.Expectations;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
 import org.junit.Test;
 import org.zinutils.hfs.FakeHFSFolder;
 import org.zinutils.hfs.FakeHierarchicalFileSystem;
+import org.zinutils.hfs.HFSFolderMatcher;
 
 public class TestUiCollectionDir {
+	@Rule public JUnitRuleMockery context = new JUnitRuleMockery();
 	FakeHierarchicalFileSystem hfs = new FakeHierarchicalFileSystem();
+	ErrorReporter errors = context.mock(ErrorReporter.class);
+	CompileUnit compiler = context.mock(CompileUnit.class);
 
 	@Test
 	public void noFilesMeansNoUIDir() throws URISyntaxException {
@@ -18,7 +28,23 @@ public class TestUiCollectionDir {
 		hfs.provideFolder(hff);
 
 		URI uri = new URI("file:/fred/bert/");
-		Root r = new Root(null, null, null, hfs, uri);
+		Root r = new Root(null, errors, null, hfs, uri);
+		r.gatherFiles();
+	}
+
+	@Test
+	public void oneFileMeansParentUIDir() throws URISyntaxException {
+		context.checking(new Expectations() {{
+			allowing(errors).logMessage(with(any(String.class)));
+			oneOf(compiler).setCardsFolder(with(HFSFolderMatcher.uri(new URI("file:/fred/bert/ui/"))));
+		}});
+		FakeHFSFolder hff = new FakeHFSFolder(new URI("file:///fred/bert/"));
+		hff.subfolder("ui").provideFile("index.html", new File("src/test/resources/lsp-files/index.html"));
+		hfs.provideFolder(hff);
+
+		URI uri = new URI("file:/fred/bert/");
+		Root r = new Root(null, errors, null, hfs, uri);
+		r.useCompiler(compiler);
 		r.gatherFiles();
 	}
 }
